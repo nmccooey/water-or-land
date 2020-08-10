a676bab510ebb19ed3f538467b7ebd856048aacd
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-08-10 12:40:58 -0400

    Updated Location.js so it renders a div with the result of the request.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import onwater from "../api/onwater";
 import SearchBar from "./SearchBar";
+import Location from "./Location";
 
 class App extends React.Component {
+  state = { onWater: false };
+
   checkUserLocation = async (lat, long) => {
     const response = await onwater.get(`${lat},${long}`);
 
-    console.log(response.data.water);
+    console.log(this.setState({ onWater: response.data.water }));
   };
 
   render() {
@@ -14,6 +17,7 @@ class App extends React.Component {
       <div className="ui container" style={{ marginTop: "30px" }}>
         <h1>Water or Land</h1>
         <SearchBar checkUserLocation={this.checkUserLocation} />
+        <Location onWater={this.state.onWater} />
       </div>
     );
   }
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,1 +1,8 @@
 import React from "react";
+
+const Location = (props) => {
+  console.log(props.onWater);
+  return <div>Result: {`${props.onWater}`}</div>;
+};
+
+export default Location;
